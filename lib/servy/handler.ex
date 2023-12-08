4453fbddd4e102ef2a663eedc03d98e4be1f611b
flowkater <chjw102@gmail.com>
2023-12-08 18:06:30 +0900

    add route pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,7 +16,12 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status_code: nil
+    }
   end
 
   def route(conv) do
@@ -24,57 +29,80 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{conv | resp_body: "Bears, Lions, Tigers"}
+    %{conv | status_code: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(conv, "GET", "/bears") do
-    %{conv | resp_body: "Teddy, Smokey, Paddington"}
+    %{conv | status_code: 200, resp_body: "Teddy, Smokey, Paddington"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{conv | status_code: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(conv, "DELETE", "/bears/" <> _id) do
+    %{conv | status_code: 403, resp_body: "Deleting bears is forbidden!"}
+  end
+
+  def route(conv, _method, path) do
+    %{conv | status_code: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     # TODO: Use Values in the map to create an HTTP response string:
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status_code} #{status_reason(conv.status_code)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
 
-request = """
-GET /wildthings HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
+# request = """
+# GET /wildthings HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
 
-"""
+# """
 
-response = Servy.Handler.handle(request)
-IO.puts(response)
+# response = Servy.Handler.handle(request)
+# IO.puts(response)
 
-request = """
-GET /bears HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
+# request = """
+# GET /bears HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
 
-"""
+# """
 
-response = Servy.Handler.handle(request)
-IO.puts(response)
+# response = Servy.Handler.handle(request)
+# IO.puts(response)
 
-request = """
-GET /bigfoot HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
+# request = """
+# GET /bigfoot HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
 
-"""
+# """
 
-response = Servy.Handler.handle(request)
-IO.puts(response)
+# response = Servy.Handler.handle(request)
+# IO.puts(response)
 
 # expected_response = """
 # HTTP/1.1 200 OK
--- test/servy_test.exs
+++ test/servy_test.exs
@@ -2,7 +2,55 @@ defmodule ServyTest do
   use ExUnit.Case
   doctest Servy
 
-  test "greets the world" do
-    assert Servy.hello() == :world
+  test "GET /bears" do
+    response = handle_request("GET", "/bears")
+    assert String.contains?(response, "200")
+    assert String.contains?(response, "OK")
+    assert String.contains?(response, "Teddy, Smokey, Paddington")
+  end
+
+  test "GET /wildthings" do
+    response = handle_request("GET", "/wildthings")
+    assert String.contains?(response, "200")
+    assert String.contains?(response, "OK")
+    assert String.contains?(response, "Bears, Lions, Tigers")
+  end
+
+  test "GET /bigfoot" do
+    response = handle_request("GET", "/bigfoot")
+    assert String.contains?(response, "404")
+    assert String.contains?(response, "Not Found")
+    assert String.contains?(response, "No /bigfoot here!")
+  end
+
+  test "GET /bears/1" do
+    response = handle_request("GET", "/bears/1")
+    assert String.contains?(response, "200")
+    assert String.contains?(response, "OK")
+    assert String.contains?(response, "Bear 1")
+  end
+
+  test "DELETE /bears/1" do
+    response = handle_request("DELETE", "/bears/1")
+    assert String.contains?(response, "403")
+    assert String.contains?(response, "Forbidden")
+    assert String.contains?(response, "Deleting bears is forbidden!")
+  end
+
+  defp handle_request(method, path) do
+    """
+    #{method} #{path} HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+
+    """
+    |> Servy.Handler.handle()
+    |> log_response()
+  end
+
+  defp log_response(response) do
+    IO.puts(response)
+    response
   end
 end
